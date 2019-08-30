60fc38cdbfbaca94f58a53810d7173d736cd3961
Karen Flores <karen.ag.flores@gmail.com>
2019-08-30 22:29:06 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,12 +1,19 @@
 class Dog
-  @@dogs = []
+  attr_accessor :name, :breed, :age
+  @@all = []
+  
   def initialize
     @name = name
     @breed = breed
     @age = age
   end
 
+  def self.create
+    dog = Dog.new(name)
+    @@all << dog
+  end
+
   def self.all
-    @@dogs
+    @@all
   end
 end
